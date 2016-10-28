3225e95acef38edfe7ffeea788eef4e31c3f1a1a
Erica Sadun <erica@ericasadun.com>
2016-10-28 15:20:03 -0600

    Major redesign

--- CGSize+Utility.swift
+++ CGSize+Utility.swift
@@ -33,47 +33,47 @@ extension CGSize {
 
 extension CGSize {
     /// Scales size by multiplying factor
-    public static  func * (size: CGSize, factor: CGFloat) -> CGSize {
+    public static func * (size: CGSize, factor: CGFloat) -> CGSize {
         return CGSize(width: size.width * factor, height: size.height * factor)
     }
     
     /// Scales size by multiplication
-    public static  func * (size: CGSize, scale: CGScale) -> CGSize {
+    public static func * (size: CGSize, scale: CGScale) -> CGSize {
         return CGSize(width: size.width * scale.sx, height: size.height * scale.sy)
     }
     
     /// Scales size by dividing factor
-    public static  func / (size: CGSize, factor: CGFloat) -> CGSize {
+    public static func / (size: CGSize, factor: CGFloat) -> CGSize {
         return CGSize(width: size.width / factor, height: size.height / factor)
     }
     
     /// Scales size by division
-    public static  func / (size: CGSize, scale: CGScale) -> CGSize {
+    public static func / (size: CGSize, scale: CGScale) -> CGSize {
         return CGSize(width: size.width / scale.sx, height: size.height / scale.sy)
     }
     
     /// Adds sizes
-    public static  func + (size1: CGSize, size2: CGSize) -> CGSize {
+    public static func + (size1: CGSize, size2: CGSize) -> CGSize {
         return CGSize(width: size1.width + size2.width, height: size1.height + size2.height)
     }
     
     /// Subtract sizes
-    public static  func - (size1: CGSize, size2: CGSize) -> CGSize {
+    public static func - (size1: CGSize, size2: CGSize) -> CGSize {
         return CGSize(width: size1.width - size2.width, height: size1.height - size2.height)
     }
     
     /// Add scalar to size
-    public static  func + (size: CGSize, factor: CGFloat) -> CGSize {
+    public static func + (size: CGSize, factor: CGFloat) -> CGSize {
         return CGSize(width: size.width + factor, height: size.height + factor)
     }
     
     /// Subtract scalar from size
-    public static  func - (size: CGSize, factor: CGFloat) -> CGSize {
+    public static func - (size: CGSize, factor: CGFloat) -> CGSize {
         return CGSize(width: size.width - factor, height: size.height - factor)
     }
     
     /// Add vector to size
-    public static  func + (size: CGSize, vector: CGVector) -> CGSize {
+    public static func + (size: CGSize, vector: CGVector) -> CGSize {
         return CGSize(width: size.width + vector.dx, height: size.height + vector.dy)
     }
     
